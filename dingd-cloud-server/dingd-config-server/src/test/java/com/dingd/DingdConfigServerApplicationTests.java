6fb169c3cb05829bc5c360dd708fda519424e5f8
weiqi <weiqi@shebei120.com>
2020-10-19 10:07:48 +0800

    第一次提交

---
+++ dingd-cloud-server/dingd-config-server/src/test/java/com/dingd/DingdConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dingd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DingdConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
